84ee1563f27456b166d92913fda1e3de27613a70
DESKTOP-RDR6UDO\Black <jonnyliudev@gmail.com>
2022-04-18 06:19:16 -0700

    ContactUs added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
+import "./App.css";
 import "react-bootstrap";
 import "font-awesome/scss/font-awesome.scss";
-import 'bootstrap/scss/bootstrap.scss';
+import "bootstrap/scss/bootstrap.scss";
 import "./assets/styles/style-min.css";
 import "./assets/styles/custom.css";
 import "slick-carousel/slick/slick.css";
@@ -12,26 +12,30 @@ import TopRow from "./Component/TopRow";
 import WhatDoWeDo from "./Component/WhatDoWeDo";
 import Collaboration from "./Component/Collaboration";
 import CollaborationContract from "./Component/CollaborationContract";
-import ShowCase from './Component/ShowCase';
-import Testimonial from './Component/Testimonial';
-import SuccessfulBanner from './Component/SuccessfulBanner';
-import Footer from './Component/Footer';
+import ShowCase from "./Component/ShowCase";
+import Testimonial from "./Component/Testimonial";
+import SuccessfulBanner from "./Component/SuccessfulBanner";
+import Footer from "./Component/Footer";
+
+import ContactUs from "./page/ContactUs";
 
 function App() {
   return (
+    /*
     <div className="App home-page-body">
       <Header></Header>
-      <div className='main'>
+      <div className="main">
         <TopRow></TopRow>
         <WhatDoWeDo></WhatDoWeDo>
         <Collaboration></Collaboration>
         <CollaborationContract></CollaborationContract>
         <ShowCase />
-        {/* <Testimonial /> */}
         <SuccessfulBanner />
       </div>
       <Footer />
     </div>
+    */
+    <ContactUs></ContactUs>
   );
 }
 
---
+++ src/page/ContactUs.js
@@ -0,0 +1,85 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import "./ContactUs.scss";
+
+export default () => {
+  const [changeHeader, setChangeHeader] = useState("");
+
+  useEffect(() => {
+    window.addEventListener("scroll", (event) => {
+      let scrollTop = event.srcElement.body.scrollTop;
+      if (scrollTop >= 85) setChangeHeader("-fixed");
+      else setChangeHeader("");
+    });
+  }, []);
+  return (
+    <>
+      <div className={`header${changeHeader}`}>
+        <div className="content-container">
+          <div className="logo">
+            <img src={`${process.env.PUBLIC_URL}/logo.png`}></img>
+          </div>
+          <div className="button talk">Let's talk</div>
+        </div>
+      </div>
+      <div className="main-container">
+        <div className="content-container">
+          <div className="heading">
+            <div className="line1">Let's Do It, Great</div>
+            <div className="line2">Be With Us, Work Together</div>
+          </div>
+          <div className="content">
+            <div className="content-left">
+              <div className="heading">Tell Us About Yourself</div>
+
+              <div className="form-container">
+                <input placeholder="Your Name"></input>
+                <input placeholder="Company Name"></input>
+                <input placeholder="Email"></input>
+                <input placeholder="Phone Number"></input>
+                <textarea
+                  cols="40"
+                  rows="10"
+                  aria-required="true"
+                  aria-invalid="false"
+                  placeholder="Project Describe"
+                ></textarea>
+                <div className="button submit-enabled">Submit</div>
+              </div>
+            </div>
+            <div className="content-right">
+              <div className="heading">or Get In Touch</div>
+              <div className="contact-line">
+                <div className="img-container">
+                  <img
+                    src={`${process.env.PUBLIC_URL}/phone-contact.png`}
+                    alt="icon"
+                  />
+                </div>
+                <div className="text">+86 173 4101-1982</div>
+              </div>
+              <div className="contact-line">
+                <div className="img-container">
+                  <img
+                    src={`${process.env.PUBLIC_URL}/email-contact.png`}
+                    alt="icon"
+                  />
+                </div>
+                <div className="text">sales@lotustech.com</div>
+              </div>
+              <div className="contact-line">
+                <div className="img-container">
+                  <img
+                    src={`${process.env.PUBLIC_URL}/box-contact.png`}
+                    alt="icon"
+                  />
+                </div>
+                <div className="text">jobs@lotustech.com</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
